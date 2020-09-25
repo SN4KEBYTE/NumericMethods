9a7c1e852daa30cf4f7989ae8661939fa718545c
Polisika <44740576+Polisika@users.noreply.github.com>
2020-09-25 23:25:24 +0700

    gauss

--- LLT/MatrixGenerator.cpp
+++ LLT/MatrixGenerator.cpp
@@ -1,33 +1,67 @@
-#include "Matrix.cpp"
+#include <vector>
+#include <fstream>
+#include <random>
 
-template<typename T>
 class MatrixGenerator
 {
 public:
-   void Gilbert(std::ofstream out, int dim)
+   void Gilbert(std::ostream& out, int dim)
    {
-      out << dim;
+      out << dim << std::endl;
 
       // di
       for (int i = 1; i <= dim; i++)
-         out << 1 / (2 * i - 1) << " ";
+         out << 1.0 / (2.0 * i - 1) << " ";
       out << std::endl;
 
       out << 0 << " ";
       // ia
-      for (int i = 0, s = 0; i < dim; i++)
-         out << s + i << " ";
+      for (int i = 0, s = 0; i < dim; i++, s += i)
+         out << s << " ";
       out << std::endl;
 
       // al
       for (int i = 1; i <= dim; i++)
          for (int j = 1; j < i; j++)
-            out << 1 / (i + j - 1);
+            out << 1.0 / (i + j - 1.0) << " ";
       out << std::endl;
 
       // au
       for (int i = 1; i <= dim; i++)
          for (int j = 1; j < i; j++)
-            out << 1 / (i + j - 1);
+            out << 1.0 / (i + j - 1) << " ";
+      out << std::endl;
+   }
+
+   void Ak(std::ostream& out, int dim, int k)
+   {
+      std::random_device rd;
+      std::mt19937 gen(rd());
+      std::uniform_int_distribution<> dist(0, 4);
+
+      out << dim << std::endl;
+      out.precision(k);
+      out.setf(std::ios::fixed);
+
+      std::vector<double> di(dim);
+      for (int p = 0; p < dim; p++)
+      {
+         double elem = 0;
+         for (int i = 0; i < dim; i++)
+            elem -= dist(gen);
+         if (p == 0)
+            elem += pow(10, -k);
+         out << elem << " ";
+         di[p] = elem;
+      }
+      out << std::endl;
+
+      for (int i = 0; i < dim + 1; i++)
+         out << 0 << " ";
+      out << std::endl;
+
+      for (int i = 1; i <= dim; i++)
+         out << di[i - 1] * i << " ";
+      out << std::endl;
    }
 };
--- LLT/Source.cpp
+++ LLT/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Matrix.cpp"
+#include "MatrixGenerator.cpp"
 #include <string>
 
 using namespace std;
@@ -7,6 +8,7 @@ using namespace std;
 #pragma region Constants, that can be changed
 
 #define type float
+const double EPS = 1e-6; // FLOAT 1e-6, DOUBLE 1e-14
 const int numTest = 3;
 
 #pragma endregion
@@ -46,7 +48,7 @@ void ShowVector(vector<T>& v, ostream& out)
 #pragma region Operations with matrix
 
 template<typename T>
-vector<T>& MatrixDotVector(Matrix<T> m, vector<T> v)
+vector<T>& MatrixDotVector(Matrix<T>& m, vector<T>& v)
 {
    vector<T> res(m.getDimension());
    for (int i = 0; i < m.getDimension(); i++)
@@ -55,27 +57,90 @@ vector<T>& MatrixDotVector(Matrix<T> m, vector<T> v)
    return *(new vector<T>(res));
 }
 
+template<typename T>
+int gauss(vector<vector<T>>& A, vector<T>& x) {
+   int n = (int)A.size();
+   int m = (int)A[0].size() - 1;
+
+   vector<int> where(m, -1);
+   for (int col = 0, row = 0; col < m && row < n; ++col) {
+      int sel = row;
+      for (int i = row; i < n; ++i)
+         if (abs(A[i][col]) > abs(A[sel][col]))
+            sel = i;
+      if (abs(A[sel][col]) < EPS)
+         continue;
+      for (int i = col; i <= m; ++i)
+         swap(A[sel][i], A[row][i]);
+      where[col] = row;
+
+      for (int i = 0; i < n; ++i)
+         if (i != row) {
+            double c = A[i][col] / A[row][col];
+            for (int j = col; j <= m; ++j)
+               A[i][j] -= A[row][j] * c;
+         }
+      ++row;
+   }
+
+   x.assign(m, 0);
+   for (int i = 0; i < m; ++i)
+      if (where[i] != -1)
+         x[i] = A[where[i]][m] / A[where[i]][i];
+   for (int i = 0; i < n; ++i) {
+      double sum = 0;
+      for (int j = 0; j < m; ++j)
+         sum += x[j] * A[i][j];
+      if (abs(sum - A[i][m]) > EPS)
+         return 0;
+   }
+
+   for (int i = 0; i < m; ++i)
+      if (where[i] == -1)
+         return INFINITY;
+   return 1;
+}
+
+template<typename T>
+void input(ifstream& in, vector<vector<T>>& A)
+{
+   int dim = 0;
+   in >> dim;
+   A.resize(dim);
+
+   for (int i = 0; i < dim; i++)
+   {
+      A[i].resize(dim + 1);
+      for (int j = 0; j < dim + 1; j++)
+         in >> A[i][j];
+   }
+}
+
 #pragma endregion
 
 #pragma region Main
 int main()
 {
-   Matrix<type> m;
-   vector<type> b;
-   // Input.
-   ifstream in(pathIn);
-   m.input(in, b);
-   in.close();
+   //ofstream out("matrixAK.txt");
+   //m.Gilbert(out, 4);
+   //Matrix<type> m;
+   //vector<type> b;
+   //// Input.
+   //ifstream in("matrixAK.txt");//pathIn);
+   //m.input(in, b);
+   //in.close();
 
-   // Solving.
-   m.factorization();
-   m.forward(b);
-   m.backward(b);
+   //// Solving.
+   //m.factorization();
+   //m.forward(b);
+   //m.backward(b);
 
-   // Output and clear memory.
-   m.~Matrix();
-   ofstream out(pathOut);
-   ShowVector<type>(b, cout);
-   out.close();
+   //// Output and clear memory.
+   //m.~Matrix();
+   //ofstream out("out.txt");
+   //cout.precision(10);
+   //cout.setf(std::ios::fixed);
+   //ShowVector<type>(b, cout);
+   //out.close();
 }
 #pragma endregion
